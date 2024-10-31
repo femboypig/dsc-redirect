f282e1af832ec982161d34911f779d915f2a0e4e
femboypig <harimassaa@gmail.com>
2024-11-01 03:35:04 +0400

    shit code and shit files fuuuck

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("joinButton").addEventListener("click", function() {
+    window.location.href = "https://discord.gg/YOUR_INVITE_CODE"; // Replace with actual invite link
+  });
+  
