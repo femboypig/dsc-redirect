ba311d9aa5340ba2de11523f396527f1d8679bf1
femboypig <harimassaa@gmail.com>
2024-11-01 03:35:39 +0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("joinButton").addEventListener("click", function() {
-    window.location.href = "https://discord.gg/YOUR_INVITE_CODE"; // Replace with actual invite link
-  });
-  
